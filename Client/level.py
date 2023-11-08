00cb7b85361793cc5d09b72765cc664d094474d9
Matheus Maydana <mattmaydana@gmail.com>
2023-11-08 01:56:09 -0300

    map - glass and floor

--- Client/level.py
+++ Client/level.py
@@ -4,74 +4,82 @@ from pygame.sprite import AbstractGroup
 from settings import *
 from tile import Tile
 from player import Player
+from helper import *
+from random import choice
 from debug import debug
 
 class Level:
-    def __init__(self, received_map):
+	def __init__(self):
 
-        # get the display surface
-        self.display_surface = pygame.display.get_surface()
+		# get the display surface 
+		self.display_surface = pygame.display.get_surface()
 
+		# sprite group setup
+		self.visible_sprites = YSortCameraGroup()
+		self.obstacle_sprites = pygame.sprite.Group()
 
-        # sprit group setup
-        self.visible_sprites = YSortCameraGroup()
-        self.obstacle_sprites = pygame.sprite.Group()
+		# sprite setup
+		self.create_map()
 
-        self.received_map = received_map
+	def create_map(self):
+		layouts = {
+			'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
+			'grass': import_csv_layout('./map/map_Grass.csv'),
+			'object': import_csv_layout('./map/map_Objects.csv'),
+		}
+		graphics = {
+			'grass': import_folder('./graphics/grass'),
+			'objects': import_folder('./graphics/objects')
+		}
 
-        # sprite setup
-        self.create_map()
+		for style,layout in layouts.items():
+			for row_index,row in enumerate(layout):
+				for col_index, col in enumerate(row):
+					if col != '-1':
+						x = col_index * TILESIZE
+						y = row_index * TILESIZE
+						if style == 'boundary':
+							Tile((x,y),[self.obstacle_sprites],'invisible')
+						if style == 'grass':
+							random_grass_image = choice(graphics['grass'])
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
 
-    def update_map(self, received_map):
-        self.received_map = received_map
-        self.create_map()
+						if style == 'object':
+							surf = graphics['objects'][int(col)]
+							Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'object',surf)
 
-    def create_map(self):
-        self.visible_sprites.empty()  # Limpa os sprites antes de criar novos
+		self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
+	def run(self):
+		# update and draw the game
+		self.visible_sprites.custom_draw(self.player)
+		self.visible_sprites.update()
 
-        for row_index, row in enumerate(self.received_map):
-            for col_index, col in enumerate(row):
-                x = col_index * TILE_SIZE
-                y = row_index * TILE_SIZE
-
-                if col == 'x':
-                    Tile(
-                        pos=(x, y),
-                        groups=[self.visible_sprites, self.obstacle_sprites]
-                    )
-                
-                if col == 'p':
-                    self.player = Player(
-                        pos=(x, y),
-                        groups=[self.visible_sprites],
-                        obstacle_sprites=self.obstacle_sprites
-                    )
-
-
-    def run(self):
-
-        # update sprites
-        self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
 
 class YSortCameraGroup(pygame.sprite.Group):
-    def __init__(self):
+	def __init__(self):
 
-        # general setup
-        super().__init__()
-        self.display_surface = pygame.display.get_surface()
+		# general setup 
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		self.half_width = self.display_surface.get_size()[0] // 2
+		self.half_height = self.display_surface.get_size()[1] // 2
+		self.offset = pygame.math.Vector2()
 
-        self.half_screen_width = self.display_surface.get_size()[0] // 2
-        self.half_screen_height = self.display_surface.get_size()[1] // 2
+		# creating the floor
+		self.floor_surf = pygame.image.load('./graphics/tilemap/ground.png').convert()
+		self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
 
-        self.offset = pygame.math.Vector2(100, 200)
+	def custom_draw(self,player):
 
-    def custom_draw(self, player):
+		# getting the offset 
+		self.offset.x = player.rect.centerx - self.half_width
+		self.offset.y = player.rect.centery - self.half_height
 
-        # getting offset
-        self.offset.x = player.rect.centerx - self.half_screen_width
-        self.offset.y = player.rect.centery - self.half_screen_height
+		# drawing the floor
+		floor_offset_pos = self.floor_rect.topleft - self.offset
+		self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
-        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+		# for sprite in self.sprites():
+		for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image,offset_pos)
--- Client/player.py
+++ Client/player.py
@@ -7,26 +7,16 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
 
         self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
-        self.rect = self.image.get_rect(topleft=pos)
-
-        self.hitbox = self.rect.inflate(0, -26)
+        self.rect = self.image.get_rect(topleft = pos)
+        self.hitbox = self.rect.inflate(0,-26)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
         self.obstacle_sprites = obstacle_sprites
 
-        self.last_position = self.direction
-
     def input(self):
         keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        elif keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        else:
-            self.direction.x = 0
 
         if keys[pygame.K_UP]:
             self.direction.y = -1
@@ -35,24 +25,30 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.y = 0
 
-    def move(self, speed):
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self,speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
-        
+
         self.hitbox.x += self.direction.x * speed
         self.collision('horizontal')
         self.hitbox.y += self.direction.y * speed
         self.collision('vertical')
-        
         self.rect.center = self.hitbox.center
+        
 
-    def collision(self, direction):
+    def collision(self,direction):
         if direction == 'horizontal':
             for sprite in self.obstacle_sprites:
                 if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.x > 0: # moving right
                         self.hitbox.right = sprite.hitbox.left
-                    
                     if self.direction.x < 0: # moving left
                         self.hitbox.left = sprite.hitbox.right
 
@@ -61,7 +57,6 @@ class Player(pygame.sprite.Sprite):
                 if sprite.hitbox.colliderect(self.hitbox):
                     if self.direction.y > 0: # moving down
                         self.hitbox.bottom = sprite.hitbox.top
-
                     if self.direction.y < 0: # moving up
                         self.hitbox.top = sprite.hitbox.bottom
 
