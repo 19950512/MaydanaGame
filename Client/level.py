4be17f25836931cb1d575cffb75f5ffd81a68b3a
Matheus Maydana <mattmaydana@gmail.com>
2023-11-07 22:27:34 -0300

    foi adicionado efeito camera
    -

--- Client/level.py
+++ Client/level.py
@@ -1,4 +1,6 @@
+from typing import Iterable, Union
 import pygame
+from pygame.sprite import AbstractGroup
 from settings import *
 from tile import Tile
 from player import Player
@@ -12,7 +14,7 @@ class Level:
 
 
         # sprit group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         self.received_map = received_map
@@ -49,5 +51,27 @@ class Level:
     def run(self):
 
         # update sprites
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+        self.half_screen_width = self.display_surface.get_size()[0] // 2
+        self.half_screen_height = self.display_surface.get_size()[1] // 2
+
+        self.offset = pygame.math.Vector2(100, 200)
+
+    def custom_draw(self, player):
+
+        # getting offset
+        self.offset.x = player.rect.centerx - self.half_screen_width
+        self.offset.y = player.rect.centery - self.half_screen_height
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
